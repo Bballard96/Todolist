aa9620dbf77c092ce2d8086fb8b5ba2abd6a4c6b
Brendan96 <Brendan.ballard96@yahoo.com>
2023-06-27 12:24:20 -0500

    add list to ejs page

--- app.js
+++ app.js
@@ -9,32 +9,21 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   let today = new Date()
-  let currentDay = today.getDay()
-  let day = ""
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+
+  let day = today.toLocaleDateString("en-US", options)
+
+  res.render("list", {
+    kindOfDay: day
+  })
 
 
-  if (currentDay == 0 ) {
-    day = "Sunday"
-    res.render("list", {kindOfDay: day })
-  } else if (currentDay == 1) {
-    day = "Monday"
-    res.render("list", {kindOfDay: day })
-  } else if (currentDay == 2) {
-    day = "Tuesday"
-    res.render("list", {kindOfDay: day })
-  } else if (currentDay == 3) {
-    day = "Wednesday"
-    res.render("list", {kindOfDay: day })
-  } else if (currentDay == 4) {
-    day = "Thursday"
-    res.render("list", {kindOfDay: day })
-  } else if (currentDay == 5) {
-    day = "Friday"
-    res.render("list", {kindOfDay: day })
-  } else if (currentDay == 6) {
-    day = "Saturday"
-    res.render("list", {kindOfDay: day })
-  }})
+  })
 
 
 app.listen(3000, function(){
