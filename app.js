58d345bbb02a499c20f3c09ad94018ff661ed4f5
Brendan96 <Brendan.ballard96@yahoo.com>
2023-06-27 18:45:51 -0500

    add list function

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+let items = []
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
   let today = new Date()
@@ -18,11 +22,16 @@ app.get('/', function(req, res){
 
   let day = today.toLocaleDateString("en-US", options)
 
-  res.render("list", {
-    kindOfDay: day
+  res.render("list", {kindOfDay: day, newListItems: items} )
+
   })
 
+  app.post('/', function(req, res) {
+    let item = req.body.newItem
 
+    items.push(item)
+
+    res.redirect('/')
   })
 
 
